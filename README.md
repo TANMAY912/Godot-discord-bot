23c5196c5f18eb545c89da8d6db174a0ee1c0bae
avanish269 <avanish269@gmail.com>
2021-09-04 04:36:28 +0530

    1. Modified commands for easier use on phone 2. Set up automatic monitoring for all channels that the bot has permission to monitor

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 - Uses python discord module
 - Personal bot for tooFunnySphere
 - Named after Godot from Waiting for Godot (Fun fact for those interested in philosophy :p)
+- Easy to use commands for phone
+- Bot now monitors every channel automatically after bootup
 
 ## Basic Commands
-- !spam: spams the best emote ever
-- !loop: sends a fixed message periodically in the channel
-- !stop: stops the loop running in the channel
-- !ping <user> <number(optional)>: Tags the mentioned user specified number of times, 7 times if no amount is specified
-- !monitor: The bots starts to monitor status of every member in the server
-- !getstatus <user>: Returns the time when the user was last online
+- !spam or ! spam: spams the best emote ever
+- !loop or ! loop: sends a fixed message periodically in the channel
+- !stop or ! stop: stops the loop running in the channel
+- !ping <user> <number(optional)> or ! ping <user> <number(optional)>: Tags the mentioned user specified number of times, 7 times if no amount is specified
+- !monitor or ! monitor: The bots starts to monitor status of every member in the server (Deprecated)
+- !getstatus <user> or ! getstatus <user>: Returns the time when the user was last online
 
 ## More Infos
 - Coming Soon
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
-import time
+from datetime import datetime
+import pytz
 import re
 import os
 
@@ -14,6 +15,25 @@ client = discord.Client(intents=intents)
 coroutineDict = dict()
 statusDict = dict()
 
+async def bootup():
+    await client.wait_until_ready()
+    print("Started")
+    global monitorFlag
+    monitorFlag = True
+    allChannels = client.get_all_channels()
+    for channel in allChannels:
+        if str(channel.type) == 'text':
+            try:
+                # await channel.send('Monitoring every member')
+                # await channel.send('<a:tooFunnySphere:872414709167583272>')
+                client.loop.create_task(statusLooper(channel))
+            except:
+                pass
+    print("Startup processes completed")
+
+@client.event
+async def on_ready():
+    print('Bot Online')
 
 async def messageLooper(message):
     while True:
@@ -21,31 +41,28 @@ async def messageLooper(message):
         await message.channel.send(msg)
         await asyncio.sleep(32400)
 
-async def statusLooper(message):
+async def statusLooper(channel):
     while True:
-        for member in message.guild.members:
+        for member in channel.guild.members:
             if str(member.status) == 'online':
-                statusDict[member] = time.ctime(time.time() + 19800)
-                # await message.channel.send(str(member) + ' -> ' + str(time.ctime(time.time())))
+                statusDict[member] = datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%B %d, %Y %H:%M:%S %Z')
         await asyncio.sleep(60)
-
-monitorFlag = False
 
 @client.event
 async def on_message(message):
-    global monitorFlag
+
     if message.author == client.user:
         return
 
     if '<a:tooFunnySphere:872414709167583272>' in message.content:
         await message.channel.send('<a:tooFunnySphere:872414709167583272>')
 
-    if message.content.startswith('!spam'):
+    if message.content.startswith('!spam') or message.content.startswith('! spam'):
         for i in range(5):
             msg = '<a:tooFunnySphere:872414709167583272>'
             await message.channel.send(msg)
 
-    if message.content.startswith('!commands'):
+    if message.content.startswith('!commands') or message.content.startswith('! commands'):
         # info_string = ('List of commands:\n'
         #                 '!spam: spams the best emote ever\n'
         #                 '!loop: sends a fixed message periodically in the channel\n'
@@ -55,26 +72,26 @@ async def on_message(message):
         embed=discord.Embed(title="**Godot Bot Info**", url="https://www.youtube.com/watch?v=xvFZjo5PgG0", description="A discord bot made for fun\nCommands start with `!`", color=0x0088ff)
         embed.set_thumbnail(url="https://lh3.googleusercontent.com/pw/AM-JKLXxpXJbLWT7lnK59M4-lPVZvlk00CIxreXN6epYWXS1N852nWJr8n1JUTLAV8Nwu2v7O1X1AnW4hEZpzfvIpoXY0NgeAuAWSiSqOxBCn_CtvwZZQmAlwsHyNhhzf_DAts0KSWptcvxroHbrsKllpkqV=s795-no")
         embed.add_field(name="\u200b", value="**List of commands**", inline=False)
-        embed.add_field(name="`!spam`", value="> Spams the best emote ever", inline=False)
-        embed.add_field(name="`!loop`", value="> Sends a fixed message periodically in the channel", inline=False)
-        embed.add_field(name="`!stop`", value="> Stops the loop running in the channel", inline=False)
-        embed.add_field(name="`!ping <user> <number(optional)>`", value="> Tags the mentioned user specified number of times, 7 times if no amount is specified", inline=False)
-        embed.add_field(name="`!monitor`", value="> The bots starts to monitor status of every member in the server", inline=False)
-        embed.add_field(name="`!getstatus <user>`", value="> Returns the time when the user was last online", inline=False)
+        embed.add_field(name="`!spam or ! spam`", value="> Spams the best emote ever", inline=False)
+        embed.add_field(name="`!loop or ! loop`", value="> Sends a fixed message periodically in the channel", inline=False)
+        embed.add_field(name="`!stop or ! stop`", value="> Stops the loop running in the channel", inline=False)
+        embed.add_field(name="`!ping <user> <number(optional)> or ! ping <user> <number(optional)>`", value="> Tags the mentioned user specified number of times, 7 times if no amount is specified", inline=False)
+        embed.add_field(name="`!monitor or ! monitor`", value="> The bots starts to monitor status of every member in the server", inline=False)
+        embed.add_field(name="`!getstatus <user> or ! getstatus <user>`", value="> Returns the time when the user was last online", inline=False)
         await message.channel.send(embed = embed)
 
-    if message.content.startswith('!botinfo'):
+    if message.content.startswith('!botinfo') or message.content.startswith('! botinfo'):
         await message.channel.send('No info for you')
         await message.channel.send('<a:PaimonTantrum:874692011490414642>')
 
-    if message.content.startswith('!loop'):
+    if message.content.startswith('!loop') or message.content.startswith('! loop'):
         if message.channel in coroutineDict.keys():
             await message.channel.send('A loop is already running on this channel')
         else:
             task = asyncio.ensure_future(messageLooper(message))
             coroutineDict[message.channel] = task
 
-    if message.content.startswith('!stop'):
+    if message.content.startswith('!stop') or message.content.startswith('! stop'):
         if message.channel in coroutineDict.keys():
             coroutineDict[message.channel].cancel()
             del coroutineDict[message.channel]
@@ -82,50 +99,91 @@ async def on_message(message):
         else:
             await message.channel.send('There is no loop running on this channel')
 
-    if message.content.startswith('!ping'):
+    if message.content.startswith('!ping') or message.content.startswith('! ping'):
         # print("ok")
         spl = message.content.rsplit()
+        # print(spl)
         # print(len(spl))
-        if len(spl) == 2:
-            # print(spl[1])
-            if spl[1][0:2] != '<@' or spl[1][-1] != '>':
-                await message.channel.send('{0.author.mention}'.format(message) + ' Type a valid username')
-                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
-            else:
-                for i in range(7):
-                    await message.channel.send(spl[1])
-
-        elif len(spl) == 3:
-            try:
-                count = int(spl[2])
-                if count >= 10:
-                    await message.channel.send('{0.author.mention}'.format(message) + ' Calm down, that\'s too many pings')
+        if spl[0] == '!ping':
+            if len(spl) == 2:
+                # print(spl[1])
+                if spl[1][0:2] != '<@' or spl[1][-1] != '>':
+                    await message.channel.send('{0.author.mention}'.format(message) + ' Type a valid username')
+                    await message.channel.send('<a:PaimonTantrum:874692011490414642>')
                 else:
-                    if spl[1][0:2] != '<@' or spl[1][-1] != '>':
+                    for i in range(7):
+                        await message.channel.send(spl[1])
+
+            elif len(spl) == 3:
+                try:
+                    count = int(spl[2])
+                    if count >= 10:
+                        await message.channel.send('{0.author.mention}'.format(message) + ' Calm down, that\'s too many pings')
+                    else:
+                        if spl[1][0:2] != '<@' or spl[1][-1] != '>':
+                            await message.channel.send('{0.author.mention}'.format(message) + ' Type a valid username')
+                            await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+                        else:
+                            for i in range(count):
+                                await message.channel.send(spl[1])
+                except Exception as e:
+                    await message.channel.send('{0.author.mention}'.format(message) + ' Enter a valid integer')
+                    await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+
+            else:
+                await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
+                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+        elif spl[0] == '!':
+                if len(spl) == 3:
+                    # print(spl[1])
+                    if spl[2][0:2] != '<@' or spl[2][-1] != '>':
                         await message.channel.send('{0.author.mention}'.format(message) + ' Type a valid username')
                         await message.channel.send('<a:PaimonTantrum:874692011490414642>')
                     else:
-                        for i in range(count):
-                            await message.channel.send(spl[1])
-            except Exception as e:
-                await message.channel.send('{0.author.mention}'.format(message) + ' Enter a valid integer')
-                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+                        for i in range(7):
+                            await message.channel.send(spl[2])
 
-        else:
-            await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
-            await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+                elif len(spl) == 4:
+                    try:
+                        count = int(spl[3])
+                        if count >= 10:
+                            await message.channel.send('{0.author.mention}'.format(message) + ' Calm down, that\'s too many pings')
+                        else:
+                            if spl[2][0:2] != '<@' or spl[2][-1] != '>':
+                                await message.channel.send('{0.author.mention}'.format(message) + ' Type a valid username')
+                                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+                            else:
+                                for i in range(count):
+                                    await message.channel.send(spl[2])
+                    except Exception as e:
+                        await message.channel.send('{0.author.mention}'.format(message) + ' Enter a valid integer')
+                        await message.channel.send('<a:PaimonTantrum:874692011490414642>')
 
-    if message.content.startswith('!monitor'):
-        if len(message.content.rsplit()) != 1:
-            await message.channel.send('Extra arguments provided <a:PaimonTantrum:874692011490414642>')
-        elif monitorFlag:
-            await message.channel.send('The server is already being monitored')
-            # await message.channel.send('<a:tooFunnySphere:872414709167583272>')
-        else:
-            monitorFlag = True
-            await message.channel.send('Monitoring every member')
-            await message.channel.send('<a:tooFunnySphere:872414709167583272>')
-            await statusLooper(message)
+                else:
+                    await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
+                    await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+
+
+    if message.content.startswith('!monitor') or message.content.startswith('! monitor'):
+        spl = message.content.rsplit()
+        if spl[0] == '!monitor':
+            if len(spl) != 1:
+                await message.channel.send('Extra arguments provided <a:PaimonTantrum:874692011490414642>')
+            elif monitorFlag:
+                await message.channel.send('The server is already being monitored')
+                # await message.channel.send('<a:tooFunnySphere:872414709167583272>')
+            else:
+                await message.channel.send('Monitoring every member')
+                await message.channel.send('<a:tooFunnySphere:872414709167583272>')
+        elif spl[0] == '!':
+            if len(spl) != 2:
+                await message.channel.send('Extra arguments provided <a:PaimonTantrum:874692011490414642>')
+            elif monitorFlag:
+                await message.channel.send('The server is already being monitored')
+                # await message.channel.send('<a:tooFunnySphere:872414709167583272>')
+            else:
+                await message.channel.send('Monitoring every member')
+                await message.channel.send('<a:tooFunnySphere:872414709167583272>')
         # for guild in client.guilds:
         # for member in message.guild.members:
             # await message.channel.send(str(member) + ' -> ' + str(member.status))
@@ -134,33 +192,52 @@ async def on_message(message):
     #     print(type(message.author))
 
 
-    if message.content.startswith('!getstatus'):
+    if message.content.startswith('!getstatus') or message.content.startswith('! getstatus'):
 
         spl = message.content.rsplit()
         # print(len(spl))
-        if len(spl) == 2:
-            # await message.channel.send(spl[1] == message.author)
-            # await message.channel.send(message.author)
-            # await message.channel.send(spl[1])
-            # print(spl[1])
-            # await message.channel.send(client.get_user(int(re.sub("[^0-9]", "", spl[1]))) == message.author)
-            # print(int(re.sub("[^0-9]", "", spl[1])))
-            if client.get_user(int(re.sub("[^0-9]", "", spl[1]))) in statusDict.keys():
-                await message.channel.send('Last online: ' + str(statusDict[client.get_user(int(re.sub("[^0-9]", "", spl[1])))]))
+        if spl[0] == '!getstatus':
+            if len(spl) == 2:
+                # await message.channel.send(spl[1] == message.author)
+                # await message.channel.send(message.author)
+                # await message.channel.send(spl[1])
+                # print(spl[1])
+                # await message.channel.send(client.get_user(int(re.sub("[^0-9]", "", spl[1]))) == message.author)
+                # print(int(re.sub("[^0-9]", "", spl[1])))
+                if client.get_user(int(re.sub("[^0-9]", "", spl[1]))) in statusDict.keys():
+                    await message.channel.send('Last online: ' + str(statusDict[client.get_user(int(re.sub("[^0-9]", "", spl[1])))]))
+                else:
+                    await message.channel.send("The user has been inactive since the bot started monitoring")
+
+                # for member in message.guild.members:
+
+                    # await message.channel.send(str(member) + ' -> ' + str(member.status))
+                #     if member = spl[1]
+
             else:
-                await message.channel.send("The user has been inactive since the bot started monitoring")
+                await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
+                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+        elif spl[0] == '!':
+            if len(spl) == 3:
+                # await message.channel.send(spl[1] == message.author)
+                # await message.channel.send(message.author)
+                # await message.channel.send(spl[1])
+                # print(spl[1])
+                # await message.channel.send(client.get_user(int(re.sub("[^0-9]", "", spl[1]))) == message.author)
+                # print(int(re.sub("[^0-9]", "", spl[1])))
+                if client.get_user(int(re.sub("[^0-9]", "", spl[2]))) in statusDict.keys():
+                    await message.channel.send('Last online: ' + str(statusDict[client.get_user(int(re.sub("[^0-9]", "", spl[2])))]))
+                else:
+                    await message.channel.send("The user has been inactive since the bot started monitoring")
 
-            # for member in message.guild.members:
+                # for member in message.guild.members:
 
-                # await message.channel.send(str(member) + ' -> ' + str(member.status))
-            #     if member = spl[1]
+                    # await message.channel.send(str(member) + ' -> ' + str(member.status))
+                #     if member = spl[1]
 
-        else:
-            await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
-            await message.channel.send('<a:PaimonTantrum:874692011490414642>')
+            else:
+                await message.channel.send('{0.author.mention}'.format(message) + ' follow the format')
+                await message.channel.send('<a:PaimonTantrum:874692011490414642>')
 
-@client.event
-async def on_ready():
-    print('Bot online')
-
+client.loop.create_task(bootup())
 client.run(TOKEN)
